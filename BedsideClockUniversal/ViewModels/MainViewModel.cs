ba9184d9e8b95067ce429a66c69239a6c799b488
David Underhill <d.underhill@techsmith.com>
2015-09-27 14:54:33 -0400

    updates

---
+++ BedsideClockUniversal/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Windows.UI.Xaml;
+using GalaSoft.MvvmLight;
+
+namespace BedsideClockUniversal.ViewModels
+{
+   public class MainViewModel : ViewModelBase
+   {
+
+      public MainViewModel()
+      {
+         _timer.Interval = TimeSpan.FromSeconds( 1 );
+         _timer.Tick += _timer_Tick;
+         _timer.Start();
+      }
+
+      DispatcherTimer _timer = new DispatcherTimer();
+      private string _backgroundImage = "ms-appx:///Assets/20131128_033722000_iOS.jpg";
+      private string _currentTime = "Test me";
+
+
+      public string BackgroundImage
+      {
+         get
+         {
+            return _backgroundImage;
+         }
+         set
+         {
+            _backgroundImage = value;
+            RaisePropertyChanged();
+         }
+      }
+      public string CurrentTime
+      {
+         get
+         {
+            return _currentTime;
+         }
+         set
+         {
+            _currentTime = value;
+            RaisePropertyChanged();
+         }
+      }
+
+
+      private void _timer_Tick( object sender, object e )
+      {
+         CurrentTime = DateTime.Now.ToString();
+      }
+
+   }
+}
---
+++ BedsideClockUniversal/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace BedsideClockUniversal.ViewModels
+{
+   public class ViewModelLocator
+   {
+      public ViewModelLocator()
+      {
+         ServiceLocator.SetLocatorProvider( () => SimpleIoc.Default );
+
+
+
+         SimpleIoc.Default.Register<MainViewModel>();
+      }
+
+      public MainViewModel MainViewModel
+      {
+         get
+         {
+            return SimpleIoc.Default.GetInstance<MainViewModel>();
+         }
+      }
+   }
+
+}
